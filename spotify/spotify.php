d1e3a77dbb55371d5c262f81c63aa225d11d36ba
Shawn Rast <rastshawn@gmail.com>
2018-08-18 17:52:11 -0400

    user's spotify playlists now appear onscreen.

--- spotify/spotify.php
+++ spotify/spotify.php
@@ -5,56 +5,102 @@ require_once 'vendor/autoload.php';
 
 session_start();
 
-
 // Authenticate the user with oAuth. 
 require_once 'oAuth.php';
 // if the user is at this line, they should have a token. 
 $token = $_SESSION['spotifyToken'];
 
-
 ?>
 
-<html>
-
-
+<html lang="en">
   <head>
 
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     <meta name="description" content="">
 
-    <title>rastsc</title>
+    <title>Playlists</title>
 
-	</head>
-    <!-- Bootstrap core CSS -->
- <!-- Latest compiled and minified CSS -->
- <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+</head>
 
- <!-- jQuery library -->
- <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
 
- <!-- Latest compiled JavaScript -->
+<!-- Bootstrap and JQuery -->    
+<!-- Bootstrap core CSS -->
+<!-- Latest compiled and minified CSS -->
+<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script> 
 
+
+<!-- Add the spotify token to javascript so it can be used for API calls. -->
 <script>
 var token='<?php echo $token;?>';
 </script>
+
+
 <script>
-$.ajax({
-	url: 'https://api.spotify.com/v1/me/playlists',
-	method: 'GET',
-	headers: {
-		'Authorization' : 'Bearer ' + token
-	},
-	success: function(response){
-		console.log(response);
-	},
-	error: function(error){
+
+var GET_PLAYLIST_ENDPOINT = "https://api.spotify.com/v1/me/playlists";
+
+
+// Spotify's API returns paged results.
+// For users with more than 20 playlists, or playlists with more than 
+// 20 songs, this will return all of them at once. 
+function getAll(endpoint) {
+	return new Promise(function(resolve, reject) {
+		$.ajax({
+			url: endpoint, 
+			method: 'GET',
+			headers: {
+				'Authorization' : 'Bearer ' + token
+			},
+			success: function(response){
+				//console.log(response);
+				if (response.next){
+					var currentResponse = response.items;
+					getAll(response.next).then(function(response) {
+						resolve(currentResponse.concat(response));
+					}, function(error) {
+						reject(error);
+					});
+					
+				} else {
+					resolve(response.items);
+				}
+			},
+			error: function(error){
+				reject(error);
+			}
+		});
+	});
+}
+
+function getTracks(playlistID) {
+	var uri = "https://api.spotify.com/v1/playlists/" + playlistID + "/tracks";
+	return getAll(uri);
+}
+function getPlaylists() {
+	var uri = "https://api.spotify.com/v1/me/playlists";
+	return getAll(uri);
+}
+var Song = new function() {
+	this.title;
+	this.artist;
+	this.album;
+	this.id;
+}
+
+
+getPlaylists()
+	.then(function(response) {
+		response.forEach(function(playlist) {
+			$("#playlists").append(playlist.name + "<br>");
+		});
+	}, function(error) {
 		console.log(error);
-	}
-});
+	});
 </script>
 <body>
-if you see this oAuth worked
+	<div id="playlists"></div>
 </body>
 </html>
